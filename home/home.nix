40d2606f711ddfbe791d91ee004909b04f4512b8
Duncan Dean <git@dunxen.dev>
2023-05-23 10:39:51 +0200

    Update helix syntax

--- home/home.nix
+++ home/home.nix
@@ -260,44 +260,46 @@
 
   programs.helix = {
     enable = true;
-    languages = [
-      {
-        name = "rust";
-        indent = {
-          tab-width = 4;
-          unit = "\t";
-        };
-      }
-      {
-        name = "typst";
-        scope = "source.typst";
-        injection-regex = "^typ(st)?$";
-        file-types = ["typ"];
-        roots = [];
-        comment-token = "//";
-        language-server = {
-          command = "typst-lsp";
-        };
-        config = {
-          exportPdf = "onType";
-        };
-      }
-      {
-        name = "nickel";
-        indent = {
-          tab-width = 2;
-          unit = "\t";
-        };
-        scope = "source.nickel";
-        injection-regex = "^ni?c(ke)?l$";
-        file-types = ["ncl"];
-        roots = [];
-        comment-token = "#";
-        language-server = {
-          command = "nls";
-        };
-      }
-    ];
+    languages = {
+      language = [
+        {
+          name = "rust";
+          indent = {
+            tab-width = 4;
+            unit = "\t";
+          };
+        }
+        {
+          name = "typst";
+          scope = "source.typst";
+          injection-regex = "^typ(st)?$";
+          file-types = ["typ"];
+          roots = [];
+          comment-token = "//";
+          language-server = {
+            command = "typst-lsp";
+          };
+          config = {
+            exportPdf = "onType";
+          };
+        }
+        {
+          name = "nickel";
+          indent = {
+            tab-width = 2;
+            unit = "\t";
+          };
+          scope = "source.nickel";
+          injection-regex = "^ni?c(ke)?l$";
+          file-types = ["ncl"];
+          roots = [];
+          comment-token = "#";
+          language-server = {
+            command = "nls";
+          };
+        }
+      ];
+    };
     settings = {
       theme = "onedark";
       editor = {
