42e3f85fed7a59ad5817040816a576936469538e
Duncan Dean <git@dunxen.dev>
2023-09-15 11:06:29 +0200

    nix flake update & passwordFile rename

--- home/default.nix
+++ home/default.nix
@@ -7,7 +7,7 @@
       isNormalUser = true;
       home = "/home/dunxen";
       createHome = true;
-      passwordFile = "/persist/encrypted-passwords/dunxen";
+      hashedPasswordFile = "/persist/encrypted-passwords/dunxen";
       shell = pkgs.nushell;
       extraGroups = [ "wheel" "disk" "networkmanager" "libvirtd" "qemu-libvirtd" "kvm" "i2c" "plugdev" "wireshark" "docker" "vboxusers" ];
       openssh.authorizedKeys.keys = [ ];
