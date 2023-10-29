a09213d6abb46e1e9fcb4bf168fe985f873d5bdb
Duncan Dean <git@dunxen.dev>
2023-10-29 14:02:15 +0200

    Float kde-polkit-agent

--- home/programs/hypr/default.nix
+++ home/programs/hypr/default.nix
@@ -103,6 +103,7 @@
     # Example windowrule v2
     # windowrulev2 = float,class:^(alacritty)$,title:^(alacritty)$
 
+    windowrule=float,org.kde.polkit-kde-authentication-agent-1
     windowrule=float,^(alacritty)$
     windowrule=float,^(pavucontrol)$
     windowrule=center,^(alacritty)$
