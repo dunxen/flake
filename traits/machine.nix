bf125dde5a633435851f07997ea4cbe72d7e1d8e
Duncan Dean <git@dunxen.dev>
2023-11-06 07:14:01 +0200

    nix flake update

--- traits/machine.nix
+++ traits/machine.nix
@@ -1,7 +1,7 @@
 /*
   A trait for configurations which are most definitely machines
 */
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   config = {
@@ -170,45 +170,45 @@
       "L /etc/secrets - - - - /persist/secrets"
       #"L /var/lib/bluetooth - - - - /persist/var/lib/bluetooth"
     ];
-    boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
-      mkdir -p /mnt
+    # boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
+    #   mkdir -p /mnt
 
-      # We first mount the btrfs root to /mnt
-      # so we can manipulate btrfs subvolumes.
-      mount -o subvol=/ /dev/mapper/encrypt /mnt
+    #   # We first mount the btrfs root to /mnt
+    #   # so we can manipulate btrfs subvolumes.
+    #   mount -o subvol=/ /dev/mapper/encrypt /mnt
 
-      # While we're tempted to just delete /root and create
-      # a new snapshot from /root-blank, /root is already
-      # populated at this point with a number of subvolumes,
-      # which makes `btrfs subvolume delete` fail.
-      # So, we remove them first.
-      #
-      # /root contains subvolumes:
-      # - /root/var/lib/portables
-      # - /root/var/lib/machines
-      #
-      # I suspect these are related to systemd-nspawn, but
-      # since I don't use it I'm not 100% sure.
-      # Anyhow, deleting these subvolumes hasn't resulted
-      # in any issues so far, except for fairly
-      # benign-looking errors from systemd-tmpfiles.
+    #   # While we're tempted to just delete /root and create
+    #   # a new snapshot from /root-blank, /root is already
+    #   # populated at this point with a number of subvolumes,
+    #   # which makes `btrfs subvolume delete` fail.
+    #   # So, we remove them first.
+    #   #
+    #   # /root contains subvolumes:
+    #   # - /root/var/lib/portables
+    #   # - /root/var/lib/machines
+    #   #
+    #   # I suspect these are related to systemd-nspawn, but
+    #   # since I don't use it I'm not 100% sure.
+    #   # Anyhow, deleting these subvolumes hasn't resulted
+    #   # in any issues so far, except for fairly
+    #   # benign-looking errors from systemd-tmpfiles.
 
-      btrfs subvolume list -o /mnt/root |
-      cut -f9 -d' ' |
-      while read subvolume; do
-        echo "deleting /$subvolume subvolume..."
-        btrfs subvolume delete "/mnt/$subvolume"
-      done &&
-      echo "deleting /root subvolume..." &&
-      btrfs subvolume delete /mnt/root
+    #   btrfs subvolume list -o /mnt/root |
+    #   cut -f9 -d' ' |
+    #   while read subvolume; do
+    #     echo "deleting /$subvolume subvolume..."
+    #     btrfs subvolume delete "/mnt/$subvolume"
+    #   done &&
+    #   echo "deleting /root subvolume..." &&
+    #   btrfs subvolume delete /mnt/root
 
-      echo "restoring blank /root subvolume..."
-      btrfs subvolume snapshot /mnt/snapshots/root/blank /mnt/root
+    #   echo "restoring blank /root subvolume..."
+    #   btrfs subvolume snapshot /mnt/snapshots/root/blank /mnt/root
 
-      # Once we're done rolling back to a blank snapshot,
-      # we can unmount /mnt and continue on the boot process.
-      umount /mnt
-    '';
+    #   # Once we're done rolling back to a blank snapshot,
+    #   # we can unmount /mnt and continue on the boot process.
+    #   umount /mnt
+    # '';
     swapDevices = [ ];
   };
 }
