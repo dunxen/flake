9fb48ac146b9e99834ec8478bfaf488249e7bcef
Duncan Dean <git@dunxen.dev>
2023-11-06 08:27:24 +0200

    Remove gnome

--- flake.nix
+++ flake.nix
@@ -152,8 +152,7 @@
       nixosModules = {
         platforms.neon = ./platforms/neon.nix;
         platforms.brute = ./platforms/brute.nix;
-        platforms.iso-minimal = "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix";
-        platforms.iso = "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix";
+        platforms.iso = "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix";
         traits.overlay = { nixpkgs.overlays = [ self.overlays.default ]; };
         traits.base = ./traits/base.nix;
         traits.machine = ./traits/machine.nix;
--- home/home.nix
+++ home/home.nix
@@ -64,105 +64,6 @@ in
     };
   };
 
-  # Use `dconf watch /` to track stateful changes you are doing, then set them here.
-  dconf.settings = {
-    "org/gnome/Console" = {
-      theme = "auto";
-    };
-    # Desktop preferences
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      enable-hot-corners = false;
-    };
-    "org/gnome/desktop/wm/preferences" = {
-      workspace-names = [ "Main" ];
-      button-layout = "appmenu:minimize,maximize,close";
-    };
-    "org/gnome/desktop/datetime" = {
-      automatic-timezone = true;
-    };
-    "org/gnome/desktop/background" = {
-      picture-uri = "file://${./wallpapers/greyguy.svg}";
-      picture-uri-dark = "file://${./wallpapers/greyguy.svg}";
-      primary-color = "#241f31";
-      secondary-color = "#030f36";
-    };
-    "org/gnome/desktop/screensaver" = {
-      picture-uri = "file:///run/current-system/sw/share/wallpapers/gnome/blobs-d.svg";
-      primary-color = "#241f31";
-      secondary-color = "#000000";
-    };
-    "org/gnome/desktop/session" = {
-      idle-delay = mkUint32 600;
-    };
-    "org/gnome/desktop/notifications" = {
-      show-in-lock-screen = false;
-    };
-    "org/gnome/desktop/peripherals/mouse" = {
-      natural-scroll = true;
-    };
-    "org/gnome/desktop/input-sources" = {
-      sources = [ (mkTuple [ "xkb" "us" ]) ];
-    };
-    "org/gnome/mutter" = {
-      dynamic-workspaces = true;
-      edge-tiling = true;
-    };
-
-    # Shell preferences
-    "org/gnome/shell" = {
-      disable-user-extensions = false;
-      disable-extension-version-validation = true;
-      # `gnome-extensions list` for a list
-      enabled-extensions = [
-        "user-theme@gnome-shell-extensions.gcampax.github.com"
-        "Vitals@CoreCoding.com"
-        "pop-shell@system76.com"
-        "dash-to-dock@micxgx.gmail.com"
-        "appindicatorsupport@rgcjonas.gmail.com"
-      ];
-      favorite-apps = [ "firefox.desktop" "org.gnome.Nautilus.desktop" "1password.desktop" "Alacritty.desktop" "thunderbird.desktop" "obsidian.desktop" ];
-    };
-    # `gsettings get org.gnome.shell.extensions.user-theme name`
-    # "org/gnome/shell/extensions/user-theme" = {
-    #   name = "palenight";
-    # };
-    "org/gnome/shell/extensions/vitals" = {
-      show-storage = false;
-      show-voltage = true;
-      show-memory = true;
-      show-fan = true;
-      show-temperature = true;
-      show-processor = true;
-      show-network = true;
-    };
-    "org/gnome/shell/extensions/dash-to-dock" = {
-      transparency-mode = "DYNAMIC";
-    };
-    "org/gnome/shell/extensions/pop-shell" = {
-      gap-outer = mkUint32 8;
-    };
-
-    # Other preferences
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
-    };
-    # Keybindings
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      name = "Toggle Light/Dark Mode";
-      binding = "<Shift><Super>d";
-      command = "xterm -e bash /home/dunxen/.toggle-dark-mode.sh";
-    };
-    "org/gnome/settings-daemon/plugins/power" = {
-      sleep-inactive-ac-timeout = 7200;
-    };
-    "org/gnome/system/location" = {
-      enabled = true;
-    };
-  };
-
   # Packages to have installed for this profile.
   home.packages =
     with pkgs; [
@@ -207,11 +108,6 @@ in
       gitoxide
       glow
       gmsh
-      gnomeExtensions.appindicator
-      gnomeExtensions.user-themes
-      gnomeExtensions.vitals
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.pop-shell
       go
       godot_4
       guix
@@ -221,6 +117,7 @@ in
       jetbrains.rust-rover
       julia
       keybase-gui
+      kicad
       kubernetes-helm
       ledger
       ledger-live-desktop
@@ -232,22 +129,25 @@ in
       nixos-generators
       nls # Nickel Language Server
       obsidian
+      obs-studio
+      obs-studio-plugins.obs-gstreamer
+      obs-studio-plugins.obs-vkcapture
+      obs-studio-plugins.obs-pipewire-audio-capture
+      obs-studio-plugins.obs-multi-rtmp
+      obs-studio-plugins.obs-move-transition
       ookla-speedtest
       protonmail-bridge
       protonvpn-cli
       protonvpn-gui
       riff
       ripgrep
+      rofi
       rustup
       signal-desktop
       sparrow
       stack
       steam
       subfinder
-      wl-clipboard
-      mako
-      rofi
-      wofi
       tailscale
       termius
       telegram-desktop
@@ -264,15 +164,9 @@ in
       wasmtime
       wally-cli
       wget
+      wl-clipboard
       zellij
       zola
-      kicad
-      obs-studio
-      obs-studio-plugins.obs-gstreamer
-      obs-studio-plugins.obs-vkcapture
-      obs-studio-plugins.obs-pipewire-audio-capture
-      obs-studio-plugins.obs-multi-rtmp
-      obs-studio-plugins.obs-move-transition
     ];
 
   programs.home-manager.enable = true;
