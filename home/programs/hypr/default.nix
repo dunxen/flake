810a41cac802458ca59fbb48e12a70cfa20300b1
Duncan Dean <git@dunxen.dev>
2023-11-12 22:02:05 +0200

    Use spacedrive for explorer shortcut

--- home/programs/hypr/default.nix
+++ home/programs/hypr/default.nix
@@ -135,7 +135,7 @@
     bind = $mainMod SHIFT, L, exec, gtklock
     bind = $mainMod SHIFT, E, exit,
     bind = $mainMod SHIFT, N,exec,sleep 1 && hyprctl dispatch dpms off
-    bind = $mainMod, F, exec, nautilus
+    bind = $mainMod, F, exec, spacedrive
     bind = $mainMod, V, togglefloating,
     bind = $mainMod, D, exec, wofi --show drun
     bind = $mainMod, R, exec, wofi --show run
