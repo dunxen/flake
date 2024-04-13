715a971d534ec9bd897f12c11f62c6a6531a5811
Duncan Dean <git@dunxen.dev>
2024-04-13 08:24:28 +0200

    nix flake update

--- traits/headed.nix
+++ traits/headed.nix
@@ -7,7 +7,7 @@
   config = {
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.displayManager.autoLogin.enable = false;
+    services.displayManager.autoLogin.enable = false;
 
     services.desktopManager.cosmic.enable = true;
     services.displayManager.cosmic-greeter.enable = true;
