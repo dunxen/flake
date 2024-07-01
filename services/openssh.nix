a48f9c0a66489c0f8553ea77c31986e346f3341b
Duncan Dean <git@dunxen.dev>
2024-07-01 21:13:35 +0200

    Temporary fix for CVE-2024-6387

--- services/openssh.nix
+++ services/openssh.nix
@@ -5,6 +5,8 @@
     services.openssh.enable = lib.mkDefault true;
     services.openssh.settings.PasswordAuthentication = false;
     services.openssh.settings.PermitRootLogin = lib.mkForce "no";
+    # Temporary fix for CVE-2024-6387 until  https://github.com/NixOS/nixpkgs/pull/323753 merged.
+    services.openssh.settings.LoginGraceTime = 0;
 
     # We're only going to allow ssh over tailscale
     networking.firewall.allowedTCPPorts = [ 22 ];
